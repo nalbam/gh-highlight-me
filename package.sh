c29ff96eb0dbf75af4f708e2ea440c8e2ffb96bb
nalbam <me@nalbam.com>
2025-12-24 16:10:17 +0900

    Add package script for building and packaging the extension

---
+++ package.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+OS_NAME="$(uname | awk '{print tolower($0)}')"
+
+SHELL_DIR=$(dirname $0)
+
+RUN_PATH=${SHELL_DIR}
+
+REPOSITORY=${GITHUB_REPOSITORY}
+
+USERNAME=${GITHUB_ACTOR}
+REPONAME=$(echo "${REPOSITORY}" | cut -d'/' -f2)
+
+################################################################################
+
+# command -v tput > /dev/null && TPUT=true
+TPUT=
+
+_echo() {
+  if [ "${TPUT}" != "" ] && [ "$2" != "" ]; then
+    echo -e "$(tput setaf $2)$1$(tput sgr0)"
+  else
+    echo -e "$1"
+  fi
+}
+
+_result() {
+  echo
+  _echo "# $@" 4
+}
+
+_command() {
+  echo
+  _echo "$ $@" 3
+}
+
+_success() {
+  echo
+  _echo "+ $@" 2
+  exit 0
+}
+
+_error() {
+  echo
+  _echo "- $@" 1
+  exit 1
+}
+
+_replace() {
+  if [ "${OS_NAME}" == "darwin" ]; then
+    sed -i "" -e "$1" $2
+  else
+    sed -i -e "$1" $2
+  fi
+}
+
+_prepare() {
+  # chmod 755
+  find ./** | grep [.]sh | xargs chmod 755
+
+  # mkdir target
+  mkdir -p ${RUN_PATH}/target
+  mkdir -p ${RUN_PATH}/release
+}
+
+################################################################################
+
+_package() {
+  VERSION=$(cat ./VERSION)
+
+  echo $VERSION > ./target/VERSION
+
+  # Update version in manifest.json
+  cat ./manifest.json | jq '.version = $VERSION' --arg VERSION $VERSION > ./manifest.json.tmp
+  mv ./manifest.json.tmp ./manifest.json
+
+  ITEMS="manifest.json content.js highlight.css icons options.html options.js popup.html popup.js"
+
+  # mv3
+  zip -r ${RUN_PATH}/release/$REPONAME-$VERSION.zip $ITEMS
+
+  # mv2 (backup and restore manifest.json)
+  cp ./manifest.json ./manifest.json.bak
+  cat ./manifest-v2.json | jq '.version = $VERSION' --arg VERSION $VERSION > ./manifest.json
+  zip -r ${RUN_PATH}/release/$REPONAME-$VERSION-mv2.zip $ITEMS
+  mv ./manifest.json.bak ./manifest.json
+}
+
+################################################################################
+
+_prepare
+
+_package
+
+_success
